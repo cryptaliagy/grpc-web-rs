43adb2974d403afb4a6078353bec164d1edaece5
Natalia Maximo <nmaximo@microsoft.com>
2024-06-29 23:17:00 -0400

    add web clients and docker compose

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { HelloRequest } from '$lib/helloworld';
+	import { GreeterClient } from '$lib/helloworld.client';
+	import { GrpcWebFetchTransport } from '@protobuf-ts/grpcweb-transport';
+
+	let message = 'This will be replaced by the response from the server.';
+
+	const sendRequest = async () => {
+		let transport = new GrpcWebFetchTransport({ baseUrl: 'http://localhost:8080' });
+		let client = new GreeterClient(transport);
+
+		let request = HelloRequest.create({ name: 'Client' });
+
+		try {
+			let response = await client.sayHello(request);
+			message = response.response.message;
+		} catch (error) {
+			message = `Error: ${error}`;
+		}
+	};
+</script>
+
+<h1>Example Application for Testing Envoy Integration</h1>
+
+<p>{message}</p>
+<button on:click={sendRequest}>Send Request</button>
